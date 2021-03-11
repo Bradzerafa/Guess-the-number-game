e4c5044fffa6948b879c4bc7933b5b8f89c70207
Brad <bradleyzerafa1@hotmail.com>
2021-03-11 21:05:54 +1100

    Readme updates + added functionality for checking users guess

--- README.md
+++ README.md
@@ -5,14 +5,22 @@
 
 ## Project Functionality
 
-    - Random number is generated and question mark is changed to the number when it is guessed.
+    - Random number is generated and question mark is changed to the number when it is guessed. [COMPLETE]
     - Check button checks the input number to the Random number.
-        - Check that the number input is between 1 - 20.
+        - Check that the number input is between 1 - 20. [COMPLETE]
         - If correct then saves the high score and changes UI.
-        - If Incorrect deducts 1 point from score.
+        - If Incorrect deducts 1 point from score. [COMPLETE]
     - Clicking the again button:
         - Hides random number and generates new random number.
         - Clears number input box.
         - Resets score back to 20.
         - Saves high score.
         - Change UI back to default look.
+
+## Notes:
+
+1. Refactor code to utilise DRY.
+2. Extra Feature:
+   - Start Screen where users enter a username.
+   - Top 3 high scores (With Usernames).
+   - Could change the score to a timer based game?
--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-'use strict';
+"use strict";
+
+const randomNumberDisplay = document.querySelector(".number");
+const guessInput = document.querySelector(".guess");
+const checkInput = document.querySelector(".check");
+const userScore = document.querySelector(".score");
+const messageResponse = document.querySelector(".message");
+
+// Random Number Generated for the player to guess.
+const randomNumber = Math.floor(Math.random() * 20 + 1);
+console.log(randomNumber);
+
+//Needs to be refactored to practice DRY/Reusability.
+//Checks the users guess against the random number to produce a result.
+function checkGuessResult() {
+  console.log(guessInput.value);
+  const guessedNumber = guessInput.value;
+  let usersCurrentScore = userScore.innerHTML;
+
+  if (guessedNumber == randomNumber) {
+    messageResponse.innerText = "You Won!";
+    randomNumberDisplay.innerText = randomNumber;
+  } else if (!guessedNumber) {
+    messageResponse.innerText = "Please enter a number";
+  } else if (guessedNumber > 20) {
+    messageResponse.innerText = "Please guess between 1 and 20";
+  } else if (guessedNumber < randomNumber) {
+    messageResponse.innerText = "Too Low";
+    usersCurrentScore = usersCurrentScore - 1;
+    userScore.innerText = usersCurrentScore;
+  } else if (guessedNumber > randomNumber) {
+    messageResponse.innerText = "Too high";
+    usersCurrentScore = usersCurrentScore - 1;
+    userScore.innerText = usersCurrentScore;
+  }
+}
+
+checkInput.addEventListener("click", checkGuessResult);
