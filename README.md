6a8e02e6036edf2f0930af02379227eef160e1f9
Brad <bradleyzerafa1@hotmail.com>
2021-03-12 11:07:36 +1100

    Functionality: All functionality complete except UI color Changes

--- README.md
+++ README.md
@@ -11,16 +11,17 @@
         - If correct then saves the high score and changes UI.
         - If Incorrect deducts 1 point from score. [COMPLETE]
     - Clicking the again button:
-        - Hides random number and generates new random number.
-        - Clears number input box.
-        - Resets score back to 20.
-        - Saves high score.
+        - Hides random number and generates new random number. [COMPLETE]
+        - Clears number input box. [COMPLETE]
+        - Resets score back to 20. [COMPLETE]
+        - Saves high score. [COMPLETE]
         - Change UI back to default look.
 
 ## Notes:
 
-1. Refactor code to utilise DRY.
-2. Extra Feature:
+1. BUG: if guess is the same it saids it's the same but still continues to check if number is lower or higher. (Will be fixed in refactor (Change large if statement to a switch statement))
+2. Refactor code to utilise DRY.
+3. Extra Feature:
    - Start Screen where users enter a username.
    - Top 3 high scores (With Usernames).
    - Could change the score to a timer based game?
--- script.js
+++ script.js
@@ -5,34 +5,63 @@ const guessInput = document.querySelector(".guess");
 const checkInput = document.querySelector(".check");
 const userScore = document.querySelector(".score");
 const messageResponse = document.querySelector(".message");
+const restartGame = document.querySelector(".again");
+const highScore = document.querySelector(".highscore");
+let prevGuess;
 
 // Random Number Generated for the player to guess.
 const randomNumber = Math.floor(Math.random() * 20 + 1);
 console.log(randomNumber);
+
+function resetStats() {
+  console.log("Hi lets reset");
+  messageResponse.innerText = "Start guessing...";
+  randomNumberDisplay.innerText = "?";
+  userScore.innerText = 20;
+  guessInput.value = "";
+}
 
 //Needs to be refactored to practice DRY/Reusability.
 //Checks the users guess against the random number to produce a result.
 function checkGuessResult() {
   console.log(guessInput.value);
   const guessedNumber = guessInput.value;
+  let gameHighScore = highScore.innerText;
   let usersCurrentScore = userScore.innerHTML;
+
+  // if guess is the same tell them.
+  if (prevGuess == guessedNumber) {
+    messageResponse.innerText = "Please guess a new number";
+    console.log("guess a different number");
+  }
 
   if (guessedNumber == randomNumber) {
     messageResponse.innerText = "You Won!";
     randomNumberDisplay.innerText = randomNumber;
+    if (usersCurrentScore > gameHighScore) {
+      console.log("New high score");
+      highScore.innerText = usersCurrentScore;
+    }
   } else if (!guessedNumber) {
     messageResponse.innerText = "Please enter a number";
   } else if (guessedNumber > 20) {
     messageResponse.innerText = "Please guess between 1 and 20";
-  } else if (guessedNumber < randomNumber) {
+  }
+
+  if (guessedNumber < randomNumber) {
     messageResponse.innerText = "Too Low";
     usersCurrentScore = usersCurrentScore - 1;
     userScore.innerText = usersCurrentScore;
+    prevGuess = guessedNumber;
+    console.log(prevGuess);
   } else if (guessedNumber > randomNumber) {
     messageResponse.innerText = "Too high";
     usersCurrentScore = usersCurrentScore - 1;
     userScore.innerText = usersCurrentScore;
+    prevGuess = guessedNumber;
+    console.log(prevGuess);
   }
 }
 
+restartGame.addEventListener("click", resetStats);
 checkInput.addEventListener("click", checkGuessResult);
